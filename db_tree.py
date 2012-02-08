034531a2e2ab305d73c878fae39d90ca1067efed
Giuseppe Sucameli <brush.tyler@gmail.com>
2012-02-08 15:50:36 +0100

    add contestual menu to database tree (fix #4928)

--- db_tree.py
+++ db_tree.py
@@ -23,13 +23,20 @@ email                : brush.tyler@gmail.com
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+from qgis.core import QgsMapLayerRegistry
+
 from .db_model import DBModel
+from .db_plugins.plugin import DBPlugin, Schema, Table
 
 class DBTree(QTreeView):
-	def __init__(self, parent=None):
-		QTreeView.__init__(self, parent)
+	def __init__(self, mainWindow):
+		QTreeView.__init__(self, mainWindow)
+		self.mainWindow = mainWindow
+
 		self.setModel( DBModel(self) )
 		self.setHeaderHidden(True)
+		self.setEditTriggers(QTreeView.EditKeyPressed|QTreeView.SelectedClicked)
+
 		self.setDragEnabled(True)
 		self.setAcceptDrops(True)
 		self.setDropIndicatorShown(True)
@@ -59,22 +66,23 @@ class DBTree(QTreeView):
 	def currentDatabase(self):
 		item = self.currentItem()
 		if item == None: return
-		try:
+
+		if isinstance(item, (DBPlugin, Schema, Table)):
 			return item.database()
-		except TypeError:	# it's a DBPlugin class object, no database
-			pass
 		return None
 
 	def currentSchema(self):
 		item = self.currentItem()
 		if item == None: return
-		if hasattr(item, 'schema'): return item.schema()
+
+		if isinstance(item, (Schema, Table)):
+			return item.schema()
 		return None
 
 	def currentTable(self):
 		item = self.currentItem()
 		if item == None: return
-		from .db_plugins.plugin import Table
+
 		if isinstance(item, Table):
 			return item
 		return None
@@ -89,4 +97,53 @@ class DBTree(QTreeView):
 
 	def currentItemChanged(self, current, previous):
 		self.itemChanged(current)
+
+	def contextMenuEvent(self, ev):
+		index = self.indexAt( ev.pos() )
+		if not index.isValid():
+			return
+
+		if index != self.currentIndex():
+			self.itemChanged(index)
+
+		item = self.currentItem()
+
+		menu = QMenu(self)
+
+		if isinstance(item, (Table, Schema)):
+			menu.addAction("Rename", self.rename)
+			menu.addAction("Delete", self.delete)
+
+			if isinstance(item, Table):
+				menu.addSeparator()
+				menu.addAction("Add to QGis canvas", self.addLayer)
+
+		elif isinstance(item, DBPlugin) and item.database() is not None:
+			menu.addAction("Re-connect", self.reconnect)
+
+		if not menu.isEmpty():
+			menu.exec_(ev.globalPos())
+
+		menu.deleteLater()
+
+	def rename(self):
+		index = self.currentIndex()
+		item = self.model.getItem(index)
+		if isinstance(item, (Table, Schema)):
+			self.edit( index )
+
+	def delete(self):
+		item = self.currentItem()
+		if isinstance(item, (Table, Schema)):
+			self.mainWindow.invokeCallback(item.database().deleteActionSlot)
+
+	def addLayer(self):
+		table = self.currentTable()
+		if table is not None:
+			QgsMapLayerRegistry.instance().addMapLayer(table.toMapLayer())
+
+	def reconnect(self):
+		db = self.currentDatabase()
+		if db is not None:
+			self.mainWindow.invokeCallback(db.reconnectActionSlot)
 
