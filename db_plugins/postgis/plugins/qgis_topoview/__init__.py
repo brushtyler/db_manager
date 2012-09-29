3ba06d50b15a62a996e72c07ba7c7263b5f37eb7
Sandro Santilli <strk@keybit.net>
2012-09-29 10:40:52 +0200

    Add lost topoviewer template project. Closes #4.

--- db_plugins/postgis/plugins/qgis_topoview/__init__.py
+++ db_plugins/postgis/plugins/qgis_topoview/__init__.py
@@ -73,7 +73,7 @@ def run(item, action, mainwindow):
 		return False
 
 	# create the new project from the template one
-	tpl_name = u'topoview_template.qgs'
+	tpl_name = u'topoview.qgs.tpl'
 	toponame = item.schema().name
 	project_name = u'topoview_%s_%s.qgs' % (uri.database(), toponame)
 
